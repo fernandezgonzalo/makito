e61e650d90fd83cb0d7cc2c7a2d8171e66165c5a
gonzadev <fernandezgonzalosebastian@gmail.com>
2020-10-09 09:08:43 -0300

    refactor

--- client/src/client.py
+++ client/src/client.py
@@ -3,40 +3,6 @@ import requests
 from http import HTTPStatus
 from hexbytes import HexBytes
 from web3.datastructures import AttributeDict
-from itertools import groupby
-
-
-def patch_web3(w3_instance, makito_client):
-    w3_instance.eth.getLogs = makito_client.get_logs
-    w3_instance.eth.getBlock = makito_client.get_block
-
-    return w3_instance
-
-
-def w3logs2makitologs(w3_logs):
-    g = groupby(w3_logs, lambda x: x.get("blockNumber"))
-    makito_logs = {
-        "blocks": []
-    }
-
-    for block_number, logs in g:
-        block = {
-            "logs": []
-        }
-        for log in logs:
-            l = {
-                "address": log.get("address"),
-                "data": log.get("data"),
-                "topics": [topic.hex() for topic in log.get("topics")],
-                "transaction_hash": log.get("transactionHash").hex()
-            }
-            block["logs"].append(l)
-        block["block_number"] = log.get("blockNumber")
-        block["block_hash"] = log.get("blockHash").hex()
-
-        makito_logs["blocks"].append(block)
-
-    return makito_logs
 
 
 class MakitoClient():
@@ -61,6 +27,7 @@ class MakitoClient():
             log["removed"] = False
             log["topics"] = [HexBytes(topic) for topic in log.get("topics")]
             log["transactionHash"] = HexBytes(log.get("transaction_hash"))
+            del log["transaction_hash"]
             log["transactionIndex"] = 1
 
 
